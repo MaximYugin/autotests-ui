d1c77b28d98e4eaf2ad1b96bb0746c882f30131a
MaximYugin <102412402+MaximYugin@users.noreply.github.com>
2025-12-07 22:08:20 +0300

    накомство с PageFactory

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,6 +1,8 @@
 from playwright.sync_api import Page, expect
 
 from components.authentication.registration_form_component import RegistrationFormComponent
+from elements.button import Button
+from elements.text import Text
 from pages.base_page import BasePage
 
 
@@ -10,13 +12,11 @@ class RegistrationPage(BasePage):
 
         self.registration_form = RegistrationFormComponent(page)
 
-
-        self.registration_button = page.get_by_test_id('registration-page-registration-button')
-        self.dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
-
+        self.registration_button = Button(page, 'registration-page-registration-button', 'Registration')
+        self.dashboard_title = Text(page, 'dashboard-toolbar-title-text', 'Dashboard')
 
     def click_registration_button(self):
         self.registration_button.click()
 
     def check_visible_dashboard_title(self):
-        expect(self.dashboard_title).to_be_visible()
+        self.dashboard_title.check_visible()
